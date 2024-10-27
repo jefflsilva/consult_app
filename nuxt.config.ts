d968f508abf515d785ad0e6f7abf9a8e181f054d
Jefferson <jeffersondevlsilva@gmail.com>
2024-10-27 19:34:21 -0300

    feat: add route on deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  app: {
+    baseURL: '/consult_app/',
+  },
   modules: [
     '@unocss/nuxt',
     '@nuxtjs/color-mode',
